80a157602e038b3c77033aa58dd99bfe8f5086f1
Zachary <zwenta@carthage.edu>
2016-12-07 21:13:56 -0600

    Re-organized javascript in the solution

--- Chat/App_Start/BundleConfig.cs
+++ Chat/App_Start/BundleConfig.cs
@@ -11,20 +11,20 @@ namespace Chat
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/javascript").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/knockout-3.4.1.min.js",
-                        "~/Scripts/modernizr-*",
-                        "~/Scripts/facebooksdk.js",
-                        "~/Scripts/bootstrap.js",
-                        "~/Scripts/cookie.js",
-                        "~/Scripts/ion.sound.min.js"));
+                        "~/Scripts/Lib/Jquery/jquery-{version}.js",
+                        "~/Scripts/Lib/Knockout/knockout-3.4.1.min.js",
+                        "~/Scripts/Lib/Modernizr/modernizr-*",
+                        "~/Scripts/Lib/Facebook/facebooksdk.js",
+                        "~/Scripts/Lib/Bootstrap/bootstrap.js",
+                        "~/Scripts/Lib/MDN/cookie.js",
+                        "~/Scripts/Lib/Ion.Sound/ion.sound.min.js"));
 
             bundles.Add(new ScriptBundle("~/chatscripts").Include(
                     "~/Scripts/chatViewModel.js"
                 ));
 
             bundles.Add(new ScriptBundle("~/chatpage").Include(
-                    "~/Scripts/jquery.signalR-2.2.1.min.js",
+                    "~/Scripts/Lib/Jquery/jquery.signalR-2.2.1.min.js",
                     "~/Scripts/server.js",
                     "~/Scripts/chatViewModel.js"
                 ));
