40fd16d908682bfb593234c5c02091af07506618
Zachary <zwenta@carthage.edu>
2017-01-29 12:39:32 -0600

    added initial webpack support?

---
+++ Chat/webpack.config.js
@@ -0,0 +1,10 @@
+﻿var path = require("path");
+
+module.exports = {
+    context: path.join(__dirname, "Scripts"),
+    entry: "./main.ts",
+    output: {
+        path: path.join(__dirname, "Scripts-Build"),
+        filename: "[name].bundle.js"
+    }
+}
